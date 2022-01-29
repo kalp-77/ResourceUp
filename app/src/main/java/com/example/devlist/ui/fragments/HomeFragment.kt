6b38247a25f16cec1ddc16aae7a0fce155cf5283
kalp <kalp07patel@gmail.com>
2022-01-30 02:20:24 +0530

    updated manifest

--- app/src/main/java/com/example/devlist/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/devlist/ui/fragments/HomeFragment.kt
@@ -1,16 +1,16 @@
 package com.example.devlist.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsControllerCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.devlist.R
 import com.example.devlist.databinding.HomeFragmentBinding
-import com.example.devlist.databinding.HomeFragmentBinding.*
+import com.example.devlist.databinding.HomeFragmentBinding.inflate
+import com.example.devlist.ui.LearnActivity
 import kotlinx.android.synthetic.main.home_fragment.*
 
 
@@ -46,7 +46,10 @@ class HomeFragment : Fragment() {
         assetsBtn.setOnClickListener {
             navController.navigate(R.id.action_homeFragment_to_assetsFragment)
         }
-
+        learnBtn.setOnClickListener {
+            val intent = Intent(context, LearnActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onDestroyView() {
